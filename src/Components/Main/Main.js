682df52f922671fd9fb4d13d23bcb72945a16466
Max <Max@bizzwc.local>
2021-11-04 05:11:49 +0500

    call pending

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import ChatWindow from "../ChatWindow/ChatWindow";
 import MessageWindow from "../MessageWindow/MessageWindow";
 import Welcome from "../Welcome/Welcome";
@@ -8,9 +8,57 @@ import { Route, Switch } from "react-router-dom";
 import { DARKMAIN } from "../../Theme/colorConstant";
 import { useSelector } from "react-redux";
 import AdminPanel from "../AdminPanel/AdminPanel";
+import Peer from 'peerjs';
 
 
 const Main = React.memo(() => {
+  const [peerId, setPeerId] = useState('');
+  const [remotePeerIdValue, setRemotePeerIdValue] = useState('');
+  const remoteVideoRef = useRef(null);
+  const currentUserVideoRef = useRef(null);
+  const peerInstance = useRef(null);
+
+  useEffect(() => {
+    const peer = new Peer();
+
+    peer.on('open', (id) => {
+      setPeerId(id)
+    });
+
+    peer.on('call', (call) => {
+      var getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+      getUserMedia({ video: false, audio: true }, (mediaStream) => {
+        currentUserVideoRef.current.srcObject = mediaStream;
+        currentUserVideoRef.current.play();
+        call.answer(mediaStream)
+        call.on('stream', function(remoteStream) {
+          remoteVideoRef.current.srcObject = remoteStream
+          remoteVideoRef.current.play();
+        });
+      });
+    })
+
+    peerInstance.current = peer;
+  }, [])
+
+  const call = (remotePeerId) => {
+    var getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+    getUserMedia({ video: false, audio: true }, (mediaStream) => {
+
+      currentUserVideoRef.current.srcObject = mediaStream;
+      currentUserVideoRef.current.play();
+
+      const call = peerInstance.current.call(remotePeerId, mediaStream)
+
+      call.on('stream', (remoteStream) => {
+        remoteVideoRef.current.srcObject = remoteStream
+        remoteVideoRef.current.play();
+      });
+    });
+  }
+
   const {isNightMode,adminPanel } = useSelector((store) => {
     return {
       isNightMode:store.app.mode || false,
@@ -18,8 +66,16 @@ const Main = React.memo(() => {
     }
   });
   return (
-    <div className="main__window" style={{background:isNightMode ? DARKMAIN : "#fff"}}>
-     VOice Call
+    <div  style={{background:isNightMode ? DARKMAIN : "#fff"}}>
+     <h1>Current user id is {peerId}</h1>
+      <input type="text" value={remotePeerIdValue} onChange={e => setRemotePeerIdValue(e.target.value)} />
+      <button onClick={() => call(remotePeerIdValue)}>Call</button>
+      <div>
+        <audio ref={currentUserVideoRef} />
+      </div>
+      <div>
+        <audio ref={remoteVideoRef} />
+      </div>
       {/* {!adminPanel && <ChatWindow />}
       <Switch>
         <Route path="/" component={Welcome} exact />
