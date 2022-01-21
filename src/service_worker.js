abfc85d7c9fc582d33d582b5266bd5363bf78023
Ahsan <ahsan.ahmed99@ymail.com>
2022-01-22 00:47:46 +0500

    PWA stuff

--- src/service_worker.js
+++ src/service_worker.js
@@ -21,7 +21,7 @@ self.addEventListener("activate", () => {
     console.log("Get Subscription", e)
   });
 })
-
+self.addEventListener('fetch',() => console.log("fetch"));
 self.addEventListener("push", (event) => {
   const notification = event.data.json();
 
@@ -44,15 +44,15 @@ self.addEventListener('notificationclick', function(event) {
   console.log('On notification click: ', event.notification);
   event.notification.close();
 
-  event.waitUntil(clients.matchAll({
-    type: "window"
-  }).then(function(clientList) {
-    for (var i = 0; i < clientList.length; i++) {
-      var client = clientList[i];
-      if (client.url == '/' && 'focus' in client)
-        return client.focus();
-    }
-    if (clients.openWindow)
-      return clients.openWindow('/');
-  }));
+  // event.waitUntil(clients.matchAll({
+  //   type: "window"
+  // }).then(function(clientList) {
+  //   for (var i = 0; i < clientList.length; i++) {
+  //     var client = clientList[i];
+  //     if (client.url == '/' && 'focus' in client)
+  //       return client.focus();
+  //   }
+  //   if (clients.openWindow)
+  //     return clients.openWindow('/');
+  // }));
 });
