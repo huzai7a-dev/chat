b02d699702527b15ff0c05954835cdd4fe1a22cc
Ahsan <ahsan.ahmed99@ymail.com>
2022-03-08 00:49:53 +0500

    Calling Drag added

--- src/Components/Call/OnCalling.jsx
+++ src/Components/Call/OnCalling.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import {
   Box,
   Typography,
@@ -18,10 +18,6 @@ import Modal from "react-modal";
 const OnCalling = React.memo((props) => {
   const [isMinimized, setMinimized] = useState(false);
   const containerRef = useRef(null);
-//   const pos1  = useRef(0);
-//   const pos2  = useRef(0);
-//   const pos3  = useRef(0);
-//   const pos4  = useRef(0);
 
   const { activeCaller } = useSelector((store) => {
     return {
@@ -31,70 +27,81 @@ const OnCalling = React.memo((props) => {
 
   const minimizedStyle = {
     content: {
-        padding:0,
-        height: "250px",
-        width: "fit-content",
+      padding: 0,
+      height: "250px",
+      width: "fit-content",
     },
     overlay: {
-        position: "absolute",
-        inset: 0,
-        height: "250px",
-        width: "fit-content",
-        backgroundColor: "rgba(255, 255, 255, 0.75)",
-      },
+      position: "absolute",
+      inset: 0,
+      height: "250px",
+      width: "fit-content",
+      backgroundColor: "rgba(255, 255, 255, 0.75)",
+      transition: 'none',
+    },
   };
 
-//   useEffect(() => {
-//     if(containerRef.current) {
-//         containerRef.current?.addEventListener("mousedown", )
-//     }
-//   }, [])
-
-//   function dragMouseDown(e) {
-//     e = e || window.event;
-//     e.preventDefault();
-//     // get the mouse cursor position at startup:
-//     pos3.current = e.clientX;
-//     pos4.current = e.clientY;
-//     document.onmouseup = closeDragElement;
-//     // call a function whenever the cursor moves:
-//     document.onmousemove = elementDrag;
-//   }
-
-//   const dragElement = (elmnt) => {
-//     pos1.current = 0;
-//     pos2.current = 0;
-//     pos3.current = 0;
-//     pos4.current = 0;
-
-//     if (document.getElementById(elmnt.id + "header")) {
-//       /* if present, the header is where you move the DIV from:*/
-//       document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-//     } else {
-//       /* otherwise, move the DIV from anywhere inside the DIV:*/
-//       elmnt.onmousedown = dragMouseDown;
-//     }
-
+  const dragElement = useCallback((elmnt) => {
     
+    let pos1 = 0,
+      pos2 = 0,
+      pos3 = 0,
+      pos4 = 0;
+    if (!elmnt) return
+    
+    /* if present, the header is where you move the DIV from:*/
+    elmnt.onmousedown = dragMouseDown;
 
-//     function elementDrag(e) {
-//       e = e || window.event;
-//       e.preventDefault();
-//       // calculate the new cursor position:
-//       pos1 = pos3 - e.clientX;
-//       pos2 = pos4 - e.clientY;
-//       pos3 = e.clientX;
-//       pos4 = e.clientY;
-//       // set the element's new position:
-//       elmnt.style.top = elmnt.offsetTop - pos2 + "px";
-//       elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
-//     }
-//   }
+    function dragMouseDown(e) {
+      e = e || window.event;
+      e.preventDefault();
+      // get the mouse cursor position at startup:
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      document.onmouseup = closeDragElement;
+      // call a function whenever the cursor moves:
+      document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+      e = e || window.event;
+      e.preventDefault();
+      // calculate the new cursor position:
+      pos1 = pos3 - e.clientX;
+      pos2 = pos4 - e.clientY;
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      // set the element's new position:
+      elmnt.style.top = elmnt.offsetTop - pos2 + "px";
+      elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
+    }
+
+    function closeDragElement() {
+      /* stop moving when mouse button is released:*/
+      document.onmouseup = null;
+      document.onmousemove = null;
+    }
+  }, []);
+
+  useEffect(() => {
+    if(!isMinimized && containerRef.current) {
+      containerRef.current.style.top = "0px";
+      containerRef.current.style.left =  "0px";
+    }
+  }, [isMinimized])
 
   return (
     <Modal
-      overlayRef={node => (containerRef.current = node)}
-      style={isMinimized ? minimizedStyle : { content:{ padding: 0 } }}
+      overlayRef={(node) => {
+        containerRef.current = node;
+        dragElement(node)
+      }}
+      style={
+        isMinimized
+          ? minimizedStyle
+          : { content: { padding: 0 }, overlay: { inset: 0, top:0,
+            left:0, } }
+      }
       isOpen={activeCaller?.elsemployees_empid}
     >
       <Box padding={0} className="onCallingContainer">
